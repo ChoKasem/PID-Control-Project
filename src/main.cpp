885be1ede7c511990493280d31268e8908a835d5
Chokpisit Kasemphaibulsuk <chokpisit.kasemphaibulsuk@mail.utoronto.ca>
2020-06-23 00:26:05 -0400

    finish tuning steering angle

--- src/main.cpp
+++ src/main.cpp
@@ -38,6 +38,8 @@ int main() {
    * TODO: Initialize the pid variable.
    */
 
+  pid.Init(0.2, 0.001, 2);
+
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -63,7 +65,12 @@ int main() {
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
-          
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
+          if(steer_value > 1) steer_value = 1;
+          else if(steer_value < -1) steer_value = -1;
+
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
                     << std::endl;
